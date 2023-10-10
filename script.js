7e418ba3a50052bc0c28ad72007d526fac2336d0
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-10-10 20:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr = str.split('');
+let result = null;
+
+for(let i = 1; i < arr.length-1; i++){
+    if(arr[i] != arr[i+1] && arr[i] != arr[i-1]){
+      result = arr[i];
+      return arr[i];
+    }
+}
+	if(arr[arr.length-1] != arr[arr.length-2]){
+		return arr[arr.length-1];
+	}
+	return result;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
